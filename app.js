5aa12f8ecd8a1e9b4e2b6582aaad728377f96179
kirmo <kirmo@DESKTOP-TK30HOS>
2022-09-15 16:36:03 +0300

    Added Proc file

--- app.js
+++ app.js
@@ -151,7 +151,11 @@ app.post("/login", (req, res)=>{
     });
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, ()=>{
-    console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started.");
 });
